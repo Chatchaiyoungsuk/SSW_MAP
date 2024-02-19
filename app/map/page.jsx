9724c21361c154693fde2d031636175747776b9b
suwizx <suwijak.pak@gmail.com>
2024-02-19 20:51:43 +0700

    clean code

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -2,18 +2,14 @@
 
 import '../map.css'
 
-import * as THREE from 'three'
 import React, { useRef, useEffect, useState } from 'react';
 import maplibregl from 'maplibre-gl';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import Header from "../components/Header";
-import { Map } from 'lucide-react';
 
 export default function Home() {
 
   const mapContainer = useRef(null);
-  const map = useRef(null);
   const [lng , setlng] = useState(99.0852798);
   const [lat , setlat] = useState(9.9485982);
   const [zoom] = useState(20);
@@ -21,11 +17,10 @@ export default function Home() {
 
   const [makerLng , setMakerLng] = useState(0)
   const [makerLat , setMakerLat] = useState(0)
-
   
   useEffect(() => {
       
-      let Map = new maplibregl.Map({
+    let Map = new maplibregl.Map({
         container: mapContainer.current,
         style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
         zoom: zoom,
@@ -33,145 +28,60 @@ export default function Home() {
         pitch: 60,
         antialias: true
     });
-// icon นะจ้ะ
+
     Map.on('load',(() => {
-      Map.loadImage('https://cdn.discordapp.com/attachments/1087745142951256134/1200722420571578368/1.png?ex=65c736e5&is=65b4c1e5&hm=a04258acb0756c1479f4c75a2ac58a08d92bf0c42165a42c101ed0087b860fe0&',(error,image) => {
-        if (error){
-          console.log(error);
-        }
-        Map.addImage('cat',image)
 
-        Map.addSource('point', {
-          'type': 'geojson',
-          'data': {
-              'type': 'FeatureCollection',
-              'features': [
-                  {
-                      'type': 'Feature',
-                      'geometry': {
-                          'type': 'Point',
-                          'coordinates': [99.0852798, 9.9485982]
-                      }
-                  }
-              ]
-          }
-      });
+        //SSW Loago 
+        Map.loadImage('https://cdn.discordapp.com/attachments/1087745142951256134/1200722420571578368/1.png?ex=65c736e5&is=65b4c1e5&hm=a04258acb0756c1479f4c75a2ac58a08d92bf0c42165a42c101ed0087b860fe0&',
+        (error,image) => {
+            
+            //handle err
+            if (error){
+            console.log(error);
+            }
 
-      Map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': 'point',
-        'layout': {
-            'icon-image': 'cat',
-            'icon-size': 0.3,
-        }
-    });
-      })
-    }))
+            //set logo ssw
+            Map.addImage('ssw',image)
 
-// model นะจ้ะ
-    const modelOrigin = [148.9819, -35.39847];
-        const modelAltitude = 0;
-        const modelRotate = [Math.PI / 2, 0, 0];
-
-        const modelAsMercatorCoordinate = maplibregl.MercatorCoordinate.fromLngLat(
-            modelOrigin,
-            modelAltitude
-        );
-
-        const modelTransform = {
-            translateX: modelAsMercatorCoordinate.x,
-            translateY: modelAsMercatorCoordinate.y,
-            translateZ: modelAsMercatorCoordinate.z,
-            rotateX: modelRotate[0],
-            rotateY: modelRotate[1],
-            rotateZ: modelRotate[2],
-            scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
-        };
-
-        let customLayer = {
-            id: '3d-model',
-            type: 'custom',
-            renderingMode: '3d',
-            onAdd(map, gl) {
-                this.camera = new THREE.Camera();
-                this.scene = new THREE.Scene();
-
-                const directionalLight = new THREE.DirectionalLight(0xffffff);
-                directionalLight.position.set(0, -70, 100).normalize();
-                this.scene.add(directionalLight);
-
-                const directionalLight2 = new THREE.DirectionalLight(0xffffff);
-                directionalLight2.position.set(0, 70, 100).normalize();
-                this.scene.add(directionalLight2);
-
-                const loader = new GLTFLoader();
-                loader.load(
-                    'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/34M_17.gltf',
-                    (gltf) => {
-                        this.scene.add(gltf.scene);
+            //set ssw logo position
+            Map.addSource('point', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': [
+                    {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [99.0852798, 9.9485982]
+                        }
                     }
-                    
-                );
-                this.map = map;
-
-                this.renderer = new THREE.WebGLRenderer({
-                    canvas: map.getCanvas(),
-                    context: gl,
-                    antialias: true
-                });
-
-                this.renderer.autoClear = false;
-            },
-            render(gl, matrix) {
-                const rotationX = new THREE.Matrix4().makeRotationAxis(
-                    new THREE.Vector3(1, 0, 0),
-                    modelTransform.rotateX
-                );
-                const rotationY = new THREE.Matrix4().makeRotationAxis(
-                    new THREE.Vector3(0, 1, 0),
-                    modelTransform.rotateY
-                );
-                const rotationZ = new THREE.Matrix4().makeRotationAxis(
-                    new THREE.Vector3(0, 0, 1),
-                    modelTransform.rotateZ
-                );
-
-                const m = new THREE.Matrix4().fromArray(matrix);
-                const l = new THREE.Matrix4()
-                    .makeTranslation(
-                        modelTransform.translateX,
-                        modelTransform.translateY,
-                        modelTransform.translateZ
-                    )
-                    .scale(
-                        new THREE.Vector3(
-                            modelTransform.scale,
-                            -modelTransform.scale,
-                            modelTransform.scale
-                        )
-                    )
-                    .multiply(rotationX)
-                    .multiply(rotationY)
-                    .multiply(rotationZ);
-
-                this.camera.projectionMatrix = m.multiply(l);
-                this.renderer.resetState();
-                this.renderer.render(this.scene, this.camera);
-                this.map.triggerRepaint();
+                ]
             }
-        };
+            });
 
-        Map.on('load', () => {
-            Map.addLayer(customLayer);
-            if(makerLng && makerLat){
-                let Marker = new maplibregl.Marker().setLngLat([makerLng,makerLat]).addTo(Map)
-            }
-        });
+            Map.addLayer({
+                'id': 'points',
+                'type': 'symbol',
+                'source': 'point',
+                'layout': {
+                    'icon-image': 'cat',
+                    'icon-size': 0.3,
+                }
+            });
 
-  }, [API_KEY, lng, lat, zoom, makerLng , makerLat]);
+        })//image Load
+
+        //Marker set
+        if(makerLng && makerLat){
+            let Marker = new maplibregl.Marker().setLngLat([makerLng,makerLat]).addTo(Map)
+        }
+
+    })
+    )// end map load
 
 
+  }, [ API_KEY , lng , lat , zoom , makerLng , makerLat ]);
  
   return (
     <>
