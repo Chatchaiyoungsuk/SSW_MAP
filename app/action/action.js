4499849dff4a889f6afa407cbf39fd8893defa7a
suwizx <suwijak.pak@gmail.com>
2024-02-21 21:47:01 +0700

    add register

--- app/action/action.js
+++ app/action/action.js
@@ -1,6 +1,7 @@
 'use server'
 
 import { PrismaClient } from '@prisma/client'
+import { hash } from 'bcryptjs'
 
 const prisma = new PrismaClient()
 
@@ -18,8 +19,17 @@ async function createUser(formData){
     const data = {
         email : formData.get("email"),
         name : formData.get("name"),
-        password : formData.get("password")
+        password : await hash(formData.get("password"),10)
     }
+    try{
+        const res = await prisma.users.create({data})
+        return {status:"sucess"}
+    }
+    catch(err){
+        console.log(err);
+        return {status:"error"}
+    }
+    
 }
 
 export { GetSites , createUser }
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -2,36 +2,46 @@
 import Link from 'next/link'
 import { useForm } from "react-hook-form";
 import { createUser } from "../action/action"
+import Header from '../components/Header';
 
 export default function Page(){
     
     const { register, handleSubmit } = useForm();
 
+    const registerHanddle = async (data) => {
+        const { name , email , password } = data
+        const formData = new FormData()
+        await formData.append("name",name)
+        await formData.append("email",email)
+        await formData.append("password",password)
+        const res = await createUser(formData)
+        if(res.status == "error"){
+            alert("เกิดข้อผิดพลาด")
+        }
+        else{
+            alert('สมัครเรียบร้แย')
+        }
+    }
+
     return(
         <>
-        <div className="p-4">
-            <form onSubmit={handleSubmit(useForm)} className="card mx-auto w-full max-w-xl bg-base-200 p-4 m-4">
-            <h1 className="text-2xl">สมัครสมาชิก</h1>
-            <div className="form-control w-full my-2">
-                <label className="label">
-                    <span className="label-text">ชื่อ - นามสกุล</span>
-                </label>
-                <input type="text" placeholder="Type here" className="input input-bordered w-full" {...register("name", { required: true})} />
-            </div>
-            <div className="form-control w-full my-2">
-                <label className="label">
-                    <span className="label-text">อีเมล</span>
-                </label>
-                <input type="email" placeholder="Type here" className="input input-bordered w-full" {...register("email", { required: true})} />
-            </div>
-            <div className="form-control w-full my-2">
-                <label className="label">
-                    <span className="label-text">รหัสผ่าน</span>
-                </label>
-                <input type="password" placeholder="Type here" className="input input-bordered w-full" {...register("password", { required: true})} />
-            </div>
-            <button type="submit" className="btn btn-primary btn-block my-2">สมัครสมาชิก</button>
-            <Link href={"/"} type="submit" className="btn btn-secondary btn-block my-2">มีบัญชีแล้ว</Link>
+        <Header />
+        <div className="p-4 flex justify-center items-center my-10">
+            <form className='max-w-xl p-4 border border-zinc-800 mx-auto w-full rounded-xl' onSubmit={handleSubmit(registerHanddle)} >
+                <h1 className='text-2xl font-bold'>สมัครสมาชิก</h1>
+                <div className="my-4">
+                    <label htmlFor="name" className='mb-2 block'>ชื่อ</label>
+                    <input {...register("name")} id='name' type="text" required className='outline-none p-2 bg-zinc-900 rounded-lg text-sm w-full focus:outline focus:outline-green-500' />
+                </div>
+                <div className="my-4">
+                    <label htmlFor="email" className='mb-2 block'>อีเมล</label>
+                    <input {...register("email")} id='email' required type="email" className='outline-none p-2 bg-zinc-900 rounded-lg text-sm w-full focus:outline focus:outline-green-500' />
+                </div>
+                <div className="my-4">
+                    <label htmlFor="password" className='mb-2 block'>รหัสผ่าน</label>
+                    <input id='password' {...register("password")} required type="password" className='outline-none p-2 bg-zinc-900 rounded-lg text-sm w-full focus:outline focus:outline-green-500' />
+                </div>
+                <button className='p-2 inline-block bg-gradient-to-tr from-green-400 to bg-yellow-400 font-bold text-black hover:shadow-lg hover:shadow-green-500/80 rounded-xl'>สมัครสมาชิก</button>
             </form>
         </div>
         </>
