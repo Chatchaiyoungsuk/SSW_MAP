60d7eaf13aad4292f428dfaf2c3c207d804014a1
Deaw_cysk <132329010+Chatchaiyoungsuk@users.noreply.github.com>
2024-02-25 11:44:59 +0700

    model work

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -7,12 +7,13 @@ import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import Header from "../components/Header";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"; // Import GLTFLoader from 'three/examples/jsm/loaders/GLTFLoader'
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader"; // Import OBJLoader from 'three/examples/jsm/loaders/OBJLoader'
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader"; // Import MTLLoader from 'three/examples/jsm/loaders/MTLLoader'
 
 export default function Home() {
   const mapContainer = useRef(null);
-  const [lng, setLng] = useState(99.0852798); // Changed setlng to setLng
-  const [lat, setLat] = useState(9.9485982); // Changed setlat to setLat
+  const [lng, setLng] = useState(99.0852798);
+  const [lat, setLat] = useState(9.9485982);
   const [zoom] = useState(20);
   const [API_KEY] = useState("0OwvauUkDP7wGwh11RM9");
 
@@ -26,13 +27,13 @@ export default function Home() {
       zoom: zoom,
       center: [lng, lat],
       pitch: 50,
-      antialias: true,
+      antialias: false,
     });
 
     map.on("load", () => {
-      // model นะจ้ะ
-      const modelOrigin = [99.0852798, 9.9485982];
-      const modelAltitude = 20;
+      // Load OBJ model with texture
+      const modelOrigin = [99.0856998, 9.9487982];
+      const modelAltitude = 0;
       const modelRotate = [Math.PI / 2, 0, 0];
 
       const modelAsMercatorCoordinate = maplibregl.MercatorCoordinate.fromLngLat(
@@ -58,21 +59,31 @@ export default function Home() {
           this.camera = new THREE.Camera();
           this.scene = new THREE.Scene();
 
-          const directionalLight = new THREE.DirectionalLight(0xffffff);
-          directionalLight.position.set(0, -70, 100).normalize();
+          const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+          directionalLight.position.set(0, -100, 10).normalize();
+          directionalLight.castShadow = false; // Disable shadow casting
           this.scene.add(directionalLight);
 
-          const directionalLight2 = new THREE.DirectionalLight(0xffffff);
-          directionalLight2.position.set(0, 70, 100).normalize();
+          const directionalLight2 = new THREE.DirectionalLight(0xffffff, 2);
+          directionalLight2.position.set(0, 100, 10).normalize();
+          directionalLight2.castShadow = false; // Disable shadow casting
           this.scene.add(directionalLight2);
 
-          const loader = new GLTFLoader(); // Changed GLTFLoader to THREE.GLTFLoader
-          loader.load(
-            "./model.gltf",
-            (gltf) => {
-              this.scene.add(gltf.scene);
-            }
-          );
+          const directionalLight3 = new THREE.DirectionalLight(0xffffff, 2);
+          directionalLight3.position.set(100, 0, 10).normalize();
+          directionalLight3.castShadow = false; // Disable shadow casting
+          this.scene.add(directionalLight3);
+
+          const mtlLoader = new MTLLoader();
+          mtlLoader.load("./ssw_model.mtl", (mtl) => {
+            mtl.preload();
+            const objLoader = new OBJLoader();
+            objLoader.setMaterials(mtl);
+            objLoader.load("./ssw_model.obj", (OBJ) => {
+              this.scene.add(OBJ);
+            });
+          });
+
           this.map = map;
 
           this.renderer = new THREE.WebGLRenderer({
@@ -127,7 +138,7 @@ export default function Home() {
 
       //SSW Logo
       map.loadImage(
-        "https://cdn.discordapp.com/attachments/1087745142951256134/1200722420571578368/1.png?ex=65c736e5&is=65b4c1e5&hm=a04258acb0756c1479f4c75a2ac58a08d92bf0c42165a42c101ed0087b860fe0&",
+        "https://cdn.discordapp.com/attachments/1087745142951256134/1200722420571578368/1.png?ex=65ec20e5&is=65d9abe5&hm=6ece3197a77950df34e175cf606c2385266e9871d951ab6ee002e7ab81e96a7f&",
         (error, image) => {
           //handle err
           if (error) {
@@ -177,8 +188,8 @@ export default function Home() {
     <>
       <div className="bg-zinc-950">
         <Header
-          setLng={setLng} // Changed setlng to setLng
-          setLat={setLat} // Changed setlat to setLat
+          setLng={setLng}
+          setLat={setLat}
           setMakerLat={setMakerLat}
           setMakerLng={setMakerLng}
         />
